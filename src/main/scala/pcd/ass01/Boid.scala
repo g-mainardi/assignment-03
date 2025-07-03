f37335e38eceda2fb6c7231dca6e19bc68506531
g-mainardi <giosue.mainardi@studio.unibo.it>
2025-07-03 12:27:27 +0200

    Implement update behavior with boids as actors

--- src/main/scala/pcd/ass01/Boid.scala
+++ src/main/scala/pcd/ass01/Boid.scala
@@ -1,28 +1,57 @@
 package pcd.ass01
 
-class Boid(private var _pos: P2d, private var _vel: V2d) {
-  import BoidsModel.Attribute
-  import Attribute.*
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
+import akka.actor.typed.{ActorRef, Behavior}
+import pcd.ass01.BoidsModel.Command.{UpdatedBoidsPos, UpdatedBoidsVel}
+
+object BoidActor:
+  enum Command:
+    case UpdateVel(model: BoidsModel, replyTo: ActorRef[BoidsModel.Command])
+    case UpdatePos(model: BoidsModel, replyTo: ActorRef[BoidsModel.Command])
+    case Kill
+
+  def apply(boid: Boid): Behavior[Command] =
+    Behaviors setup { ctx => new BoidActor(ctx, boid).waiting }
+
+case class Boid(private var _pos: P2d, private var _vel: V2d):
   def pos: P2d = _pos
   def pos_=(p: P2d): Unit = _pos = p
   def vel: V2d = _vel
   def vel_=(v: V2d): Unit = _vel = v
 
-  def updateVelocity(model: BoidsModel): Unit =
-    val calc = calculate(getNearbyBoids(model), model)
-    vel = vel + Attribute.values.foldLeft(V2d(.0, .0)): (acc, attr) =>
-      acc + (calc(attr) * (model getWeightOf attr))
-    if vel.abs > model.maxSpeed then vel = vel.norm * model.maxSpeed
+private class BoidActor(ctx: ActorContext[BoidActor.Command], val boid: Boid) {
+  import BoidsModel.Attribute
+  import Attribute.*
+  import BoidActor.Command.*
 
-  def updatePos(model: BoidsModel): Unit =
-    pos = pos + vel
-    if pos.x < model.getMinX  then pos = pos + V2d(model.width, 0)
-    if pos.x >= model.getMaxX then pos = pos + V2d(-model.width, 0)
-    if pos.y < model.getMinY  then pos = pos + V2d(0, model.height)
-    if pos.y >= model.getMaxY then pos = pos + V2d(0, -model.height)
+  export boid.{pos, vel}
+
+  private val waiting: Behavior[BoidActor.Command] = Behaviors.receiveMessagePartial:
+    case UpdateVel(model, replyTo) =>
+      updateVelocity(model)
+      replyTo ! UpdatedBoidsVel
+      Behaviors.same
+    case UpdatePos(model, replyTo) =>
+      updatePos(model)
+      replyTo ! UpdatedBoidsPos
+      Behaviors.same
+    case Kill => Behaviors.stopped
+
+  private def updateVelocity(model: BoidsModel): Unit =
+    val calc = calculate(getNearbyBoids(model), model)
+    boid.vel = vel + Attribute.values.foldLeft(V2d(.0, .0)): (acc, attr) =>
+      acc + (calc(attr) * (model getWeightOf attr))
+    if vel.abs > model.maxSpeed then boid.vel = vel.norm * model.maxSpeed
+
+  private def updatePos(model: BoidsModel): Unit =
+    boid.pos = pos + vel
+    if pos.x <  model.getMinX then boid.pos = pos + V2d(model.width, 0)
+    if pos.x >= model.getMaxX then boid.pos = pos + V2d(-model.width, 0)
+    if pos.y <  model.getMinY then boid.pos = pos + V2d(0, model.height)
+    if pos.y >= model.getMaxY then boid.pos = pos + V2d(0, -model.height)
 
   private def getNearbyBoids(model: BoidsModel) = model.boids filter : other =>
-    (other ne this) && posInRadius(other.pos, model.perceptionRadius)
+    (other ne boid) && posInRadius(other.pos, model.perceptionRadius)
 
   private def calculate(boids: Seq[Boid], model: BoidsModel)(a: Attribute): V2d = (a match
     case SEPARATION => calculateSeparation
@@ -32,7 +61,7 @@ class Boid(private var _pos: P2d, private var _vel: V2d) {
   private def calculateAll(getVector: Boid => Vector2d)(nearbyBoids: Seq[Boid], model: BoidsModel) =
     if nearbyBoids.nonEmpty
     then
-      val vec = getVector(this)
+      val vec = getVector(boid)
       val (avgX, avgY) = nearbyBoids.map(getVector)
         .foldLeft((.0,.0)){(acc, other) => (acc._1 + other.x, acc._2 + other.y)}
       V2d(avgX / nearbyBoids.size - vec.x, avgY / nearbyBoids.size - vec.y).norm
--- src/main/scala/pcd/ass01/BoidsModel.scala
+++ src/main/scala/pcd/ass01/BoidsModel.scala
@@ -1,7 +1,8 @@
 package pcd.ass01
 
 import akka.actor.typed.{ActorRef, Behavior}
-import akka.actor.typed.scaladsl.ActorContext
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
+import pcd.ass01.BoidActor.Command.*
 import pcd.ass01.BoidsSimulator.Loop
 
 object BoidsModel:
@@ -26,21 +27,30 @@ class BoidsModel(var nBoids: Int,
   import Command.*
 
   var boids: Seq[Boid] = Seq()
+  private var boidsRef: Seq[ActorRef[BoidActor.Command]] = Seq()
+  private var simRef: Option[ActorRef[Loop]] = None
 
   def generateBoids(ctx: ActorContext[Loop]): Unit =
     boids =
       for
-        i <- 0 until nBoids
+        i     <- 0 until nBoids
         origin = P2d(-width / 2, -height / 2)
-        pos = origin + V2d(Math.random * width, Math.random * height)
+        pos    = origin + V2d(Math.random * width, Math.random * height)
         rndVel = V2d(Math.random, Math.random) * (maxSpeed / 2)
-        vel = rndVel + v2d(-maxSpeed / 4)
+        vel    = rndVel + v2d(- maxSpeed / 4)
       yield
         Boid(pos, vel)
+    boidsRef =
+      for
+        (b, ind) <- boids.zipWithIndex
+      yield
+        ctx spawn (BoidActor(b), s"boid$ind")
 
   def clearBoids(): Unit =
     nBoids = 0
-    boids = List()
+    boids = Seq()
+    boidsRef foreach {_ ! Kill}
+    boidsRef = Seq()
 
   def getMinX: Double = -width / 2
   def getMaxX: Double = width / 2
@@ -59,4 +69,38 @@ class BoidsModel(var nBoids: Int,
 
   def setBoidsNumber(n: Int): Unit = nBoids = n
 
-  def updateBoids(ctx: ActorContext[Loop], replyTo: ActorRef[Loop]): Behavior[Command] = ???
+  def updateBoids(ctx: ActorContext[Loop], replyTo: ActorRef[Loop]): Behavior[Command] = Behaviors.setup[Command]: ctx =>
+    simRef = Some(replyTo)
+    startUpdatingVel(ctx)
+
+  private object NRemainingBoids:
+    def unapply(n: Int): Option[Int] = if n > 0 then Some(n - 1) else None
+
+  private def startUpdatingVel(ctx: ActorContext[Command]) =
+    boidsRef foreach:
+      _ ! UpdateVel(this, ctx.self)
+    updatingVel(nBoids)
+
+  private def updatingVel(remainingBoids: Int): Behavior[Command] = Behaviors.receivePartial:
+    case (ctx, UpdatedBoidsVel) => remainingBoids match
+      case NRemainingBoids(n) => n match
+        case 0 => startUpdatingPos(ctx)
+        case still => updatingVel(still)
+      case _ => throw new IllegalStateException("Update Vel: boid number not valid!")
+
+  private def startUpdatingPos(ctx: ActorContext[Command]) =
+    boidsRef foreach:
+      _ ! UpdatePos(this, ctx.self)
+    updatingPos(nBoids)
+
+  private def updatingPos(remainingBoids: Int): Behavior[Command] = Behaviors.receivePartial:
+    case (ctx, UpdatedBoidsPos) => remainingBoids match
+      case NRemainingBoids(n) => n match
+        case 0 => endUpdatingPos
+        case still => updatingPos(still)
+      case _ => throw new IllegalStateException("Update Pos: boid number not valid!")
+
+  private def endUpdatingPos: Behavior[Command] =
+    simRef foreach:
+      _ ! Loop.UpdateView
+    Behaviors.stopped
