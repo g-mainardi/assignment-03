da9b0b7382cf72bf0e61bab70e6e4235902228c7
g-mainardi <giosue.mainardi@studio.unibo.it>
2025-07-11 11:30:28 +0200

    Moved Simulation parameters to Simulator

--- src/main/scala/pcd/ass01/BoidsSimulation.scala
+++ src/main/scala/pcd/ass01/BoidsSimulation.scala
@@ -1,18 +1,5 @@
 package pcd.ass01
 
-object SimulationParameter:
-  val N_BOIDS = 1000
-  val SCREEN_WIDTH = 1280
-  val SCREEN_HEIGHT = 480
-  private[ass01] val SEPARATION_WEIGHT = 1.0
-  private[ass01] val ALIGNMENT_WEIGHT = 1.0
-  private[ass01] val COHESION_WEIGHT = 1.0
-  private[ass01] val ENVIRONMENT_WIDTH = 1000
-  private[ass01] val ENVIRONMENT_HEIGHT = 1000
-  private[ass01] val MAX_SPEED = 4.0
-  private[ass01] val PERCEPTION_RADIUS = 50.0
-  private[ass01] val AVOID_RADIUS = 20.0
-
 object BoidsSimulation extends App:
   import SimulationParameter.*
   val model = BoidsModel(N_BOIDS, SEPARATION_WEIGHT, ALIGNMENT_WEIGHT,
--- src/main/scala/pcd/ass01/BoidsSimulator.scala
+++ src/main/scala/pcd/ass01/BoidsSimulator.scala
@@ -3,6 +3,19 @@ import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
 import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import pcd.ass01.BoidsModel.Attribute
 
+object SimulationParameter:
+  val N_BOIDS = 1000
+  val SCREEN_WIDTH = 1280
+  val SCREEN_HEIGHT = 480
+  private[ass01] val SEPARATION_WEIGHT = 1.0
+  private[ass01] val ALIGNMENT_WEIGHT = 1.0
+  private[ass01] val COHESION_WEIGHT = 1.0
+  private[ass01] val ENVIRONMENT_WIDTH = 1000
+  private[ass01] val ENVIRONMENT_HEIGHT = 1000
+  private[ass01] val MAX_SPEED = 4.0
+  private[ass01] val PERCEPTION_RADIUS = 50.0
+  private[ass01] val AVOID_RADIUS = 20.0
+
 object BoidsSimulator :
   enum Loop:
     case Start(nBoids: String)
